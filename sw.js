d02793eaa7169752a961cef6b44ced0d3365a096
HAL-Garege <halgaragecd@gmail.com>
2026-08-31 14:54:36 -0500

    Fix: cargar exportación de Jornada y Almuerzo v2

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
-const CACHE='hal-garage-v20';
+const CACHE='hal-garage-v21';
 const CORE='./hal-core-fix.js?v=3';
 const PHOTO='./hal-photo-fix.js?v=2';
 const VEHICLE='./vehicle-category-fix.js?v=1';
 const JORNADA='./hal-jornada-legacy.js?v=3';
-const EXPORT='./hal-jornada-export.js?v=1';
+const EXPORT='./hal-jornada-export-v2.js?v=2';
 const ASSETS=['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png',CORE,PHOTO,VEHICLE,JORNADA,EXPORT];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));self.skipWaiting()});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==CACHE).map(x=>caches.delete(x)))));self.clients.claim()});
