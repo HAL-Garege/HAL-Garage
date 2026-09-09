9fdefcad2734dbde34c1b3439b9ba1f93d67dcfb
HAL-Garege <halgaragecd@gmail.com>
2026-09-09 07:46:21 -0500

    improve Club PWA service worker

--- club-hal-garage/sw.js
+++ club-hal-garage/sw.js
@@ -1,4 +1,4 @@
-const CACHE='club-hal-v1';
+const CACHE='club-hal-v2';
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(()=>self.skipWaiting()))});
-self.addEventListener('activate',event=>{event.waitUntil(self.clients.claim())});
-self.addEventListener('fetch',event=>{});
+self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k.startsWith('club-hal-')&&k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
+self.addEventListener('fetch',event=>{if(event.request.method!=='GET')return;event.respondWith(fetch(event.request).catch(()=>caches.match(event.request))) });
